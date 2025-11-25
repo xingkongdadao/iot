5a53b43a41c54f12b35324e4de23e05357fc236a
xingkongdadao <xingkongdadao@gmail.com>
2025-11-25 22:54:37 +0700

    注释

--- ESP32C3/ESP32-C3-TDM2421-4G-GPS/ESP32-C3-TDM2421-4G-GPS.ino
+++ ESP32C3/ESP32-C3-TDM2421-4G-GPS/ESP32-C3-TDM2421-4G-GPS.ino
@@ -10,6 +10,7 @@
 
 namespace {
 
+// 初始化调制解调器的自检流程，确保基本 AT 命令可用
 void initializeModemDiagnostics() {
     Serial.println("Checking AT command...");
     sim_at_cmd("AT");
@@ -23,6 +24,7 @@ void initializeModemDiagnostics() {
     sim_at_cmd("AT+CIMI");
 }
 
+// 将 USB 串口输入透传到模组串口，方便直接发送 AT 指令
 void forwardUsbToModem() {
     if (Serial.available()) {
         AppConfig::modemSerial().write(Serial.read());
@@ -32,6 +34,7 @@ void forwardUsbToModem() {
 }  // namespace
 
 void setup() {
+    // 先给 SIM 模组供电，并点亮板载 LED，提示系统启动
     pinMode(AppConfig::MCU_SIM_EN_PIN, OUTPUT);
     digitalWrite(AppConfig::MCU_SIM_EN_PIN, HIGH);
     delay(500);
@@ -42,6 +45,7 @@ void setup() {
     Serial.println("\n\n\n\n-----------------------\nSystem started!!!!");
     delay(8000);
 
+    // 初始化上传组件并尝试连接 Wi-Fi
     GeoUploader::init();
     WifiManager::begin();
     Serial.printf("Connecting to %s\n", AppConfig::WIFI_SSID);
@@ -49,6 +53,7 @@ void setup() {
         Serial.println("WiFi unavailable, will retry in loop");
     }
 
+    // 配置与蜂窝模组的串口并执行自检，再预热 GPS
     AppConfig::modemSerial().begin(
         AppConfig::MCU_SIM_BAUDRATE, SERIAL_8N1, AppConfig::MCU_SIM_RX_PIN, AppConfig::MCU_SIM_TX_PIN);
     initializeModemDiagnostics();
@@ -57,6 +62,7 @@ void setup() {
 }
 
 void loop() {
+    // 主循环中保持串口转发、网络连通以及上传逻辑
     forwardUsbToModem();
     WifiManager::ensureConnected();
     GeoUploader::flushBuffer();
